5b29ca2e64927d781f7755e3436fb0120c7cfda2
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2026-04-19 17:40:14 +0100

    Auto-generated commit

---
+++ assets/env.js
@@ -0,0 +1,12 @@
+window.__env = {
+  "firebase": {
+    "apiKey": "AIzaSyCO2ND4ZOiZatgvPozesCdAgDLGMER-fic",
+    "authDomain": "pdigs-e9c79.firebaseapp.com",
+    "projectId": "pdigs-e9c79",
+    "storageBucket": "pdigs-e9c79.firebasestorage.app",
+    "messagingSenderId": "394833869657",
+    "appId": "1:394833869657:web:b56afcb4c797cc7b842a48",
+    "measurementId": "G-H7K5YREMSB"
+  },
+  "googleMapsApiKey": "AIzaSyA8vJvACNlU6sTjvlTv_BMNwTUpBlIKdfI"
+};
